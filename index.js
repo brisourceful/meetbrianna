9523184e50c04f5571f22e6136906a9a5daec697
Brianna <briannaburrows@gmail.com>
2017-07-27 15:35:05 -0700

    updated h1 styling, ham-menu pseudo styling, uncommented js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(document).ready(function () {
+
   $('#hamburger-menu').click(function () {
     console.log('this happened');
     $(this).toggleClass('active');
@@ -9,37 +10,37 @@ $(document).ready(function () {
   });
 
 
-  // let form = $('#contact');
-  // let formMessages = $('#form-messages');
-  //
-  // $(form).submit(function(event) {
-  //   event.preventDefault();
-  //
-  //   let formData = $(form).serialize();
-  //
-  //   $.ajax({
-  //     type: 'POST',
-  //     url: $(form).attr('action'),
-  //     data: formData
-  //   })
-  //   .done(function(response) {
-  //     $(formMessages).removeClass('error');
-  //     $(formMessages).addClass('success');
-  //
-  //     $(formMessages).text(response);
-  //
-  //     $('#name').val('');
-  //     $('#email').val('');
-  //     $('#message').val('');
-  //   })
-  //   .fail(function(data) {
-  //     $(formMessages).removeClass('success');
-  //     $(formMessages).addClass('error');
-  //
-  //     if(data.responseText !== ''){
-  //       $(formMessages).text(data.responseText);
-  //     } else {
-  //       $(formMessages).text('Oops! An error occured and your message could not be sent.')
-  //     }
-  //   });
+  let form = $('#contact');
+  let formMessages = $('#form-messages');
+
+  $(form).submit(function(event) {
+    event.preventDefault();
+
+    let formData = $(form).serialize();
+
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    })
+    .done(function(response) {
+      $(formMessages).removeClass('error');
+      $(formMessages).addClass('success');
+
+      $(formMessages).text(response);
+
+      $('#name').val('');
+      $('#email').val('');
+      $('#message').val('');
+    })
+    .fail(function(data) {
+      $(formMessages).removeClass('success');
+      $(formMessages).addClass('error');
+
+      if(data.responseText !== ''){
+        $(formMessages).text(data.responseText);
+      } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.')
+      }
+    });
 });
